d8162b08898c7ad8ba42dc6918805b1ad542a3b3
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2023-11-05 13:22:08 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const url = "https://npgnuzxbhcqoawlztorp.supabase.co"
+
+const supabase = createClient(url, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ251enhiaGNxb2F3bHp0b3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg0MjIzMDAsImV4cCI6MjAxMzk5ODMwMH0.mU1IbZVdFsvDTGwPD25mrci7guzxWy582ASOAmONBi8);
 
 module.exports = supabase;
